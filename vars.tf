dd6fe437e43de4be5d8faae03208ee36b5885b3f
betillox <105825363+betillox@users.noreply.github.com>
2023-04-12 19:07:29 -0600

    Update vars.tf

--- vars.tf
+++ vars.tf
@@ -5,7 +5,7 @@ variable "aws_region" {
 
 variable "key_name" {
   description = " SSH keys to connect to ec2 instance"
-  default     =  "my-keypair"
+  default     =  "my_keypair"
 }
 
 variable "instance_type" {
