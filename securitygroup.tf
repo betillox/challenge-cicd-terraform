98df54b3fe531066d350dc1d16b73b40cef4640c
betillox <105825363+betillox@users.noreply.github.com>
2023-04-12 18:24:18 -0600

    Update securitygroup.tf

--- securitygroup.tf
+++ securitygroup.tf
@@ -1,25 +1,31 @@
-resource "aws_security_group" "demo-sg" {
-  name        = "demo-sg"
-  description = "allow ssh and http traffic"
+#Create security group with firewall rules
+resource "aws_security_group" "my_security_group" {
+  name        = var.security_group
+  description = "security group for Ec2 instance"
 
   ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+ ingress {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
-  ingress {
-    from_port   = 80
-    to_port     = 80
+ # outbound from jenkis server
+  egress {
+    from_port   = 0
+    to_port     = 65535
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
-  egress {
-    from_port       = 0
-    to_port         = 0
-    protocol        = "-1"
-    cidr_blocks     = ["0.0.0.0/0"]
+  tags= {
+    Name = var.security_group
   }
 }
