ecb14afda1c7d3092689d4777989e2e01535e3c5
betillox <albertoumanachaves@gmail.com>
2023-04-13 21:01:15 -0600

    access key change

--- vars.tf
+++ vars.tf
@@ -5,7 +5,7 @@ variable "aws_region" {
 
 variable "key_name" {
   description = " SSH keys to connect to ec2 instance"
-  default     =  "my_keypair"
+  default     =  "jenkins_access.pem"
 }
 
 variable "instance_type" {
